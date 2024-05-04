09900a1a7087a1a6a8c8661e1a450a1e1948e60c
Caleb Meyer <calebzmeyer@gmail.com>
2024-05-04 13:16:55 -0700

    Create Card Model

---
+++ models/card.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from pydantic import BaseModel
+
+from enums.rank import Rank
+from enums.suit import Suit
+
+
+class Card(BaseModel):
+
+    rank: Rank
+    suit: Suit
+
+    def __eq__(self, other: Card) -> bool:
+        if self.__class__ is not other.__class__:
+            return NotImplemented
+
+        return self.rank == other.rank and self.suit == other.suit
+
+    def __lt__(self, other: Card) -> bool:
+        if self.__class__ is not other.__class__:
+            return NotImplemented
+
+        if self.suit < other.suit:
+            return True
+        if self.suit > other.suit:
+            return False
+
+        return self.rank < other.rank
+
+    def __str__(self) -> str:
+        return f'{self.rank.symbol}{self.suit.symbol}'
---
+++ tests/models/test_card.py
@@ -0,0 +1,67 @@
+import unittest
+
+from enums.rank import Rank
+from enums.suit import Suit
+from models.card import Card
+
+
+class CardTestCase(unittest.TestCase):
+
+    def test_equal_comparison(self):
+        # Given
+        card_one = Card(rank=Rank.ACE, suit=Suit.SPADES)
+        card_two = Card(rank=Rank.ACE, suit=Suit.SPADES)
+
+        # When
+        comparison = (card_one == card_two)
+
+        # Then
+        self.assertTrue(comparison)
+
+    def test_not_equal_comparison(self):
+        # Given
+        card_one = Card(rank=Rank.ACE, suit=Suit.SPADES)
+        card_two = Card(rank=Rank.QUEEN, suit=Suit.DIAMONDS)
+
+        # When
+        comparison = (card_one != card_two)
+
+        # Then
+        self.assertTrue(comparison)
+
+    def test_less_than_comparison(self):
+        # Given
+        lesser = Card(rank=Rank.ACE, suit=Suit.SPADES)
+        greater = Card(rank=Rank.QUEEN, suit=Suit.DIAMONDS)
+
+        # When
+        comparison = (lesser < greater)
+
+        # Then
+        self.assertTrue(comparison)
+
+    def test_greater_than_comparison(self):
+        # Given
+        lesser = Card(rank=Rank.ACE, suit=Suit.SPADES)
+        greater = Card(rank=Rank.QUEEN, suit=Suit.DIAMONDS)
+
+        # When
+        comparison = (greater > lesser)
+
+        # Then
+        self.assertTrue(comparison)
+
+    def test_str(self):
+        # Given
+        card = Card(rank=Rank.ACE, suit=Suit.SPADES)
+        expected_string = "AS"
+
+        # When
+        string = str(card)
+
+        # Then
+        self.assertEqual(expected_string, string)
+
+
+if __name__ == "__main__":
+    unittest.main()
