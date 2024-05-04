25e365746a3353e1682886179c77074638da1782
Caleb Meyer <calebzmeyer@gmail.com>
2024-05-03 22:20:56 -0700

    Set Rank Enum Value to Symbol (#3)

--- enums/rank.py
+++ enums/rank.py
@@ -1,59 +1,34 @@
-from enum import IntEnum
+from __future__ import annotations
+from enum import Enum
 
 
-class Rank(IntEnum):
+class Rank(Enum):
 
-    ACE = 0
-    KING = 1
-    QUEEN = 2
-    JACK = 3
-    TEN = 4
-    NINE = 5
-    EIGHT = 6
-    SEVEN = 7
-    SIX = 8
-    FIVE = 9
-    FOUR = 10
-    THREE = 11
-    TWO = 12
+    ACE = "A"
+    KING = "K"
+    QUEEN = "Q"
+    JACK = "J"
+    TEN = "T"
+    NINE = "9"
+    EIGHT = "8"
+    SEVEN = "7"
+    SIX = "6"
+    FIVE = "5"
+    FOUR = "4"
+    THREE = "3"
+    TWO = "2"
 
-    def __str__(self) -> str:
-        return self.symbol
+    def __lt__(self, other: Rank) -> bool:
+        if self.__class__ is not other.__class__:
+            return NotImplemented
+
+        members = list(self.__class__)
+        return members.index(self) < members.index(other)
 
     @property
     def symbol(self) -> str:
-        rank_to_symbol_map = {
-            Rank.ACE: "A",
-            Rank.KING: "K",
-            Rank.QUEEN: "Q",
-            Rank.JACK: "J",
-            Rank.TEN: "T",
-            Rank.NINE: "9",
-            Rank.EIGHT: "8",
-            Rank.SEVEN: "7",
-            Rank.SIX: "6",
-            Rank.FIVE: "5",
-            Rank.FOUR: "4",
-            Rank.THREE: "3",
-            Rank.TWO: "2",
-        }
-        return rank_to_symbol_map[self]
+        return self.value
 
     @staticmethod
-    def from_symbol(symbol: str) -> "Rank":
-        symbol_to_rank_map = {
-            "A": Rank.ACE,
-            "K": Rank.KING,
-            "Q": Rank.QUEEN,
-            "J": Rank.JACK,
-            "T": Rank.TEN,
-            "9": Rank.NINE,
-            "8": Rank.EIGHT,
-            "7": Rank.SEVEN,
-            "6": Rank.SIX,
-            "5": Rank.FIVE,
-            "4": Rank.FOUR,
-            "3": Rank.THREE,
-            "2": Rank.TWO,
-        }
-        return symbol_to_rank_map[symbol]
+    def from_symbol(symbol: str) -> Rank:
+        return Rank(symbol)
