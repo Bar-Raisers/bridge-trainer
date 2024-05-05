78deae7c9cae248adda2d098f73246f15d52fdaa
Caleb Meyer <calebzmeyer@gmail.com>
2024-05-04 17:48:54 -0700

    Create Hand Model (#8)

---
+++ models/hand.py
@@ -0,0 +1,22 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+from models.card import Card
+
+
+class Hand(BaseModel):
+
+    cards: List[Card] = []
+
+    def __init__(self, cards: Optional[List[Card]] = None) -> None:
+        super().__init__(cards=cards)
+        self.cards = sorted(self.cards)
+
+    def __str__(self) -> str:
+        return ", ".join(
+            [
+                str(card)
+                for card in self.cards
+            ]
+        )
