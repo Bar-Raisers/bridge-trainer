573acbe6587e77229232eb7875d685984199afec
Caleb Meyer <calebzmeyer@gmail.com>
2024-05-09 13:12:43 -0700

    Compute Dealer Seat From Board Number (#13)

--- tests/enums/test_seat.py
+++ tests/enums/test_seat.py
@@ -16,3 +16,25 @@ class SeatTestCase(unittest.TestCase):
 
         # Then
         self.assertTrue(is_hashable)
+
+    def test_get_dealer_seat_from_board_number(self):
+        # Given
+        dealer_by_board_number = {
+            1: Seat.NORTH,
+            2: Seat.EAST,
+            3: Seat.SOUTH,
+            4: Seat.WEST,
+            5: Seat.NORTH,
+            6: Seat.EAST,
+            7: Seat.SOUTH,
+            8: Seat.WEST,
+        }
+
+        # When & Then
+        for board_number, expected_dealer in dealer_by_board_number.items():
+            dealer = Seat.get_dealer_seat_from_board_number(board_number)
+            self.assertEqual(expected_dealer, dealer)
+
+
+if __name__ == "__main__":
+    unittest.main()
