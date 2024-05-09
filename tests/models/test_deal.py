d8a5c94598ebfeaa9705c798baee98721e59cece
Caleb Meyer <calebzmeyer@gmail.com>
2024-05-09 13:21:02 -0700

    Create Deal Model (#14)

---
+++ tests/models/test_deal.py
@@ -0,0 +1,98 @@
+import unittest
+
+from enums.rank import Rank
+from enums.seat import Seat
+from enums.suit import Suit
+from enums.vulnerability import Vulnerability
+from models.card import Card
+from models.deal import Deal
+from models.hand import Hand
+
+
+class DealTestCase(unittest.TestCase):
+
+    def test_constructor_populates_dealer(self):
+        # Given
+        board_number = 1
+        expected_dealer = Seat.NORTH
+
+        # When
+        deal = Deal(
+            board_number=board_number,
+            north=Hand(cards=[]),
+            east=Hand(cards=[]),
+            south=Hand(cards=[]),
+            west=Hand(cards=[]),
+        )
+
+        # Then
+        self.assertEqual(expected_dealer, deal.dealer)
+
+    def test_constructor_populates_vulnerability(self):
+        # Given
+        board_number = 1
+        expected_vulnerability = Vulnerability.NONE
+
+        # When
+        deal = Deal(
+            board_number=board_number,
+            north=Hand(cards=[]),
+            east=Hand(cards=[]),
+            south=Hand(cards=[]),
+            west=Hand(cards=[]),
+        )
+
+        # Then
+        self.assertEqual(expected_vulnerability, deal.vulnerability)
+
+    def test_str(self):
+        # Given
+        north = Hand(cards=[Card(rank=Rank.ACE, suit=Suit.SPADES)])
+        east = Hand(cards=[Card(rank=Rank.ACE, suit=Suit.HEARTS)])
+        south = Hand(cards=[Card(rank=Rank.ACE, suit=Suit.DIAMONDS)])
+        west = Hand(cards=[Card(rank=Rank.ACE, suit=Suit.CLUBS)])
+
+        deal = Deal(
+            board_number=1,
+            north=north,
+            east=east,
+            south=south,
+            west=west,
+        )
+        expected_string = (
+            f"{Seat.NORTH}: AS\n"
+            f"{Seat.EAST}: AH\n"
+            f"{Seat.SOUTH}: AD\n"
+            f"{Seat.WEST}: AC"
+        )
+
+        # When
+        string = str(deal)
+
+        # Then
+        self.assertEqual(expected_string, string)
+
+    def test_get_hand(self):
+        # Given
+        north = Hand(cards=[Card(rank=Rank.ACE, suit=Suit.SPADES)])
+        east = Hand(cards=[Card(rank=Rank.ACE, suit=Suit.HEARTS)])
+        south = Hand(cards=[Card(rank=Rank.ACE, suit=Suit.DIAMONDS)])
+        west = Hand(cards=[Card(rank=Rank.ACE, suit=Suit.CLUBS)])
+
+        deal = Deal(
+            board_number=1,
+            north=north,
+            east=east,
+            south=south,
+            west=west,
+        )
+
+        # When
+        hand = deal.get_hand(Seat.NORTH)
+
+        # Then
+        self.assertListEqual(north.cards, hand.cards)
+
+
+if __name__ == "__main__":
+    unittest.main()
