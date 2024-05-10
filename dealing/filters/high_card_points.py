93ba98cbb97e7bbb04980cf0f4902c6681020480
Caleb Meyer <calebzmeyer@gmail.com>
2024-05-09 18:21:46 -0700

    Create HighCardPointsFilter (#17)
    * Create Base DealFilter
    
    * Create HighCardPointsFilter

---
+++ dealing/filters/high_card_points.py
@@ -0,0 +1,28 @@
+from dealing.filters.base import DealFilter
+from enums.seat import Seat
+from models.deal import Deal
+from utilities.hand import calculate_high_card_points
+
+
+class HighCardPointsFilter(DealFilter):
+
+    def __init__(
+        self,
+        seat: Seat,
+        minimum_high_card_points: int,
+        maximum_high_card_points: int,
+    ) -> None:
+        self.seat = seat
+        self.minimum_high_card_points = minimum_high_card_points
+        self.maximum_high_card_points = maximum_high_card_points
+
+    def evaluate(self, deal: Deal) -> bool:
+        hand = deal.get_hand(self.seat)
+        high_card_points = calculate_high_card_points(hand)
+
+        if high_card_points < self.minimum_high_card_points:
+            return False
+        if high_card_points > self.maximum_high_card_points:
+            return False
+
+        return True
