6eacb864cecd71c311c9eec5e6c5159dd79c099f
Caleb Meyer <calebzmeyer@gmail.com>
2024-05-12 02:00:19 +0000

    Create Deck Model

---
+++ dealing/models/deck.py
@@ -0,0 +1,41 @@
+from random import shuffle
+from typing import List
+
+from pydantic import BaseModel
+
+from enums.rank import Rank
+from enums.suit import Suit
+from models.card import Card
+
+
+class Deck(BaseModel):
+
+    cards: List[Card]
+
+    def draw(self) -> Card:
+        return self.cards.pop(0)
+
+    def is_empty(self) -> bool:
+        return len(self.cards) == 0
+
+    def shuffle(self) -> None:
+        shuffle(self.cards)
+
+
+class DeckFactory:
+
+    @staticmethod
+    def create_sorted_deck() -> Deck:
+        cards = []
+
+        for suit in list(Suit):
+            for rank in list(Rank):
+                cards.append(Card(rank=rank, suit=suit))
+
+        return Deck(cards=cards)
+
+    @classmethod
+    def create_shuffled_deck(cls) -> Deck:
+        deck = cls.create_sorted_deck()
+        deck.shuffle()
+        return deck
