e99694ac5d285b5468986676fb7664f7878ea762
Caleb Meyer <calebzmeyer@gmail.com>
2024-05-13 16:51:25 -0700

    Reorganize Parsing Module as Criteria Resource (#24)

--- dealing/resources/criteria/__init__.py
+++ dealing/resources/criteria/__init__.py
@@ -2,12 +2,12 @@ from typing import Any, Dict
 
 from dealing.enums.filters import FilterType
 from dealing.filters import DealFilter
-from dealing.parsing.high_card_points import parse_high_card_points_filter
+from dealing.resources.criteria.high_card_points import parse_high_card_points_criteria
 
 
-def parse_filter(attributes: Dict[str, Any]) -> DealFilter:
+def parse_criteria(attributes: Dict[str, Any]) -> DealFilter:
     parse_by_type = {
-        FilterType.HIGH_CARD_POINTS: parse_high_card_points_filter,
+        FilterType.HIGH_CARD_POINTS: parse_high_card_points_criteria,
     }
     filter_type = FilterType(attributes["type"])
     return parse_by_type[filter_type](attributes)
