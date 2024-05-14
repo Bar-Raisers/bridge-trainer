80e635f69730861cd0b13970bae180139726a82a
Caleb Meyer <calebzmeyer@gmail.com>
2024-05-13 17:34:06 -0700

    Load Criteria from JSON Files (#25)

--- dealing/resources/criteria/__init__.py
+++ dealing/resources/criteria/__init__.py
@@ -1,8 +1,14 @@
+import json
 from typing import Any, Dict
 
 from dealing.enums.filters import FilterType
 from dealing.filters import DealFilter
 from dealing.resources.criteria.high_card_points import parse_high_card_points_criteria
+
+
+def load_criteria_from_json_file(json_file_path: str) -> Dict[str, Any]:
+    with open(json_file_path) as json_file:
+        return json.load(json_file)
 
 
 def parse_criteria(attributes: Dict[str, Any]) -> DealFilter:
